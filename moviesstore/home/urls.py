5ac0382a89046ceb19a52d04f3ff508089ac2de1
Aryan Chadha <aryanchadha@lawn-143-215-104-215.lawn.gatech.edu>
2025-01-28 16:12:09 -0500

    Register and Login

--- moviesstore/home/urls.py
+++ moviesstore/home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import index, register, login_view, logout_view
 
 urlpatterns = [
-    path('', views.index, name='home.index'),
+    path("register/", register, name="register"),
+    path('', index, name='home'),
+    path('login/', login_view, name='login'),
+    path("logout/", logout_view, name="logout"),
 ]
--- moviesstore/home/views.py
+++ moviesstore/home/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    return render(request, "home/index.html", {"show_nav": True})
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+
+    return render(request, "home/register.html", {"form": form, "show_nav": False})
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "home/login.html", {"form": form, "show_nav": False})
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
